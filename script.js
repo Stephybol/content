86c52fd9cdcf66ba968c8065ca634b5b6193b046
Stephybol <bolognastefania85@gmail.com>
2025-06-25 09:23:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const text = ["Developer", "Designer", "Freelancer"];
+let index = 0;
+let charIndex = 0;
+let currentText = '';
+let isDeleting = false;
+
+function type() {
+  const typingElement = document.querySelector('.typing');
+
+  if (index >= text.length) index = 0;
+  currentText = text[index];
+
+  if (isDeleting) {
+    typingElement.textContent = currentText.substring(0, charIndex--);
+  } else {
+    typingElement.textContent = currentText.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === currentText.length) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    index++;
+  }
+
+  setTimeout(type, isDeleting ? 60 : 100);
+}
+
+document.addEventListener("DOMContentLoaded", type);
