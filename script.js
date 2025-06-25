417751f88599594560af358d8b2fd535885d5ca8
Stephybol <bolognastefania85@gmail.com>
2025-06-25 15:40:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const baseText = "I'm a Grance";
-const extraWords = ["Manager", "PM", "BI"];
+const baseText = "I am....";
+const extraWords = ["Service Manager", "Project Manager", "Business Intelligence"];
 let index = 0;
 let fullText = baseText;
 const typingElement = document.querySelector('.typing');
